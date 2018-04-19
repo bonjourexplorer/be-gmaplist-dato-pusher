e5a805846a610767543400d36b67cf0d605110fb
Derek Dunagan <derek.dunagan@gmail.com>
2018-04-18 20:35:44 -0600

    publish to airtable

--- source/index.js
+++ source/index.js
@@ -10,6 +10,7 @@
     function push_to_airtable(params, log) {
         const { api_key, list, place_list } = validate_params(params);
         const base = new Airtable({ apiKey: api_key }).base(CONTENT_BASE_ID);
+        const processed_place_list = [];
         let is_remote_place_list_processed = false;
 
         const list_table = base('Lists');
@@ -33,10 +34,8 @@
 
         function push(resolve) {
             const list_query = list_table.select(list_query_criteria);
-            // debugger;
             list_query.firstPage(process_list_result);
             const place_query = place_table.select(place_select_criteria);
-            // debugger;
             place_query.all(process_place_result);
             setTimeout(bail, timeout * 60 * 1000);
             return true;
@@ -44,7 +43,6 @@
             // -----------
 
             function process_list_result(exception, list_result) {
-                // debugger;
                 if (exception) {
                     throw exception;
                 }
@@ -59,7 +57,6 @@
             }
 
             function process_place_result(exception, remote_place_list) {
-                // debugger;
                 if (exception) {
                     throw exception;
                 }
@@ -67,35 +64,59 @@
                 for (const place of remote_place_list) {
                     const { title, google_place_id } = place.fields;
                     log && log(`   ${ title }`, 'verbose');
-                    remote_place_dict[google_place_id] = place.id;
+                    remote_place_dict[google_place_id] = place;
                 }
                 is_remote_place_list_processed = true;
                 return upsert_place_list();
             }
 
             function upsert_place_list() {
-                if (list_id && is_remote_place_list_processed) {
-                    log && log('⬆️  Publishing new places info');
-                    debugger;
-                    for (const place of place_list) {
-                        const { google_place_id } = place;
-                        const airtable_id = remote_place_dict[google_place_id];
-                        airtable_id
-                            ? place_table.update(airtable_id, place, finish_op)
-                            : place_table.create(place, finish_op)
-                            ; // eslint-disable-line indent
-                    }
-                    return resolve(`🏁  Finished publishing!`);
+                if (!list_id || !is_remote_place_list_processed) {
+                    return true;
+                }
+                log && log('⬆️  Publishing new places info');
+                for (const place of place_list) {
+                    const { google_place_id } = place;
+                    const remote_place = remote_place_dict[google_place_id];
+                    const airtable_id = remote_place && remote_place.id;
+                    const list_list = remote_place
+                        && remote_place.get('list_list')
+                        || []
+                        ; // eslint-disable-line indent
+                    const patch = Object.assign({}, place, {
+                        address: place.address && place.address.join('\n'),
+                        lat_long: place.coords && place.coords.join(','),
+                        website_title: place.website && place.website.title,
+                        website_url: place.website && place.website.url,
+                        be_blurb: place.user_blurb,
+                        list_list,
+                        }); // eslint-disable-line indent
+                    delete patch.coords;
+                    delete patch.website;
+                    delete patch.user_blurb;
+                    list_list.includes(list_id)
+                        || patch.list_list.push(list_id)
+                        ; // eslint-disable-line indent
+                    airtable_id
+                        ? place_table.update(airtable_id, patch, finish_op)
+                        : place_table.create(patch, finish_op)
+                        ; // eslint-disable-line indent
                 }
                 return true;
             }
 
             function finish_op(exception, place) {
-                debugger;
                 if (exception) {
                     throw exception;
                 }
+                processed_place_list.push(Object.assign(
+                    { id: place.id },
+                    place.fields),
+                    ); // eslint-disable-line indent
                 log && log(`   ${ place.get('title') }`, 'verbose');
+                if (processed_place_list.length === place_list.length) {
+                    return resolve(`🏁  Finished publishing!`);
+                }
                 return true;
             }
             function bail() {
